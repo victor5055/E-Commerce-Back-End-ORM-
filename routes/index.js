0a0fbf0348c4404b4090daa2b9782ffff8e65262
Victor <victor5055@outlook.com>
2022-12-05 17:06:15 -0500

    Adding index js for api.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const categoryRoutes = require('./category-routes');
+const productRoutes = require('./product-routes');
+const tagRoutes = require('./tag-routes');
 
-router.use('/api', apiRoutes);
+router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
 
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
 
 module.exports = router;
