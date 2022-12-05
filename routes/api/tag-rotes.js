18f6f561418e923fc1beb6872da04de88e6bbe21
Victor <victor5055@outlook.com>
2022-12-05 17:16:07 -0500

    Adding create new tag.

--- routes/api/tag-rotes.js
+++ routes/api/tag-rotes.js
@@ -66,6 +66,12 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+    Tag.create(req.body)
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
