e8b83a7caf62248916137728820b81e38cc02b11
Victor <victor5055@outlook.com>
2022-12-05 12:34:33 -0500

    Adding one catergory by its id.

--- routes/api/category-rotes.js
+++ routes/api/category-rotes.js
@@ -27,6 +27,26 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      "id",
+      "category_name"
+    ], 
+    include: [
+      {
+        model: Product, 
+        attributes: ["id", "product_name", "price", "stock", "category_id"]
+      }
+    ]
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
