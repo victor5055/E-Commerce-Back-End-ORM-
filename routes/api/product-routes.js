e03dc1ec9aa200e4e186951bc49c4722b70f5594
Victor <victor5055@outlook.com>
2022-12-05 13:30:40 -0500

    Adding product find all on product routes.:

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      "id",
+      "product_name",
+      "price",
+      "stock",
+      "category_id"
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: [ "id", "category_name" ]
+      },
+      {
+        model: Tag,
+        as: "tags",
+        attributes: [ "id", "tag_name" ]
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
